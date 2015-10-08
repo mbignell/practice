89e2f42b2ec567f7bc1e35a277e0d8c9292a5ae1
mbignell <maggie.bignell@gmail.com>
2015-10-07 22:28:37 -0700

    Initial Commit

---
+++ musicProtoTests/musicProtoTests.swift
@@ -0,0 +1,36 @@
+//
+//  musicProtoTests.swift
+//  musicProtoTests
+//
+//  Created by Margaret Bignell on 10/7/15.
+//  Copyright © 2015 maggled. All rights reserved.
+//
+
+import XCTest
+@testable import musicProto
+
+class musicProtoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
